7de0946f4ea1b3de17156f5c75071c30b423851b
Marta Kociszewska <martakociszewska04@gmail.com>
2025-10-28 20:36:06 +0100

    add queries to create data base (not tested)

---
+++ task2/BULK_loading/creating_database.sql
@@ -0,0 +1,93 @@
+CREATE DATABASE go_explore_travel_agency /* exams */
+GO
+
+USE go_explore_travel_agency
+GO
+
+CREATE TABLE Tour /* wycieczka */
+(
+    tour_id INT IDENTITY(1,1) PRIMARY KEY, /* Generates primery key, from 1 with step 1 */
+	name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+	destination VARCHAR(255) NOT NULL,
+	tour_type NVARCHAR(20) 
+	    CHECK (tour_type IN ('All-inclusive', 'Rodzinna', 'Aktywna')), /* TODO : set different acceptable values or remove this constraint */
+	attractions VARCHAR(255) NOT NULL
+)
+GO
+
+CREATE TABLE TourEdition /* turnus */
+(
+    tour_edition_id INT IDENTITY(1,1) PRIMARY KEY, /* Generates primery key, from 1 with step 1 */
+	start_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	available_slots INT NOT NULL 
+		CHECK (available_slots >= 0),
+	tour_id INT NOT NULL,
+	FOREIGN KEY (tour_id) REFERENCES Tour(tour_id) /* relacja 1:n wycieczka - turnus */
+)
+GO
+
+CREATE TABLE Worker /* pracownik */
+(
+	worker_pesel CHAR(11) PRIMARY KEY, /* Generates primery key, from 1 with step 1 */
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	email NVARCHAR(255) NOT NULL UNIQUE,
+	phone_number CHAR(9) NOT NULL UNIQUE,
+	role VARCHAR(100) NOT NULL
+)
+GO
+
+CREATE TABLE Client /* klient */
+(
+	client_pesel CHAR(11) PRIMARY KEY, /* Generates primery key, from 1 with step 1 */
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	email NVARCHAR(255) NOT NULL UNIQUE,
+	phone_number CHAR(9) NOT NULL UNIQUE
+)
+GO
+
+CREATE TABLE Reservation /* rezerwacja */
+(
+	reservation_id INT IDENTITY(1,1) PRIMARY KEY, /* Generates primery key, from 1 with step 1 */
+	reservation_date DATE NOT NULL,
+	reservation_status NVARCHAR(20) 
+	    CHECK (reservation_status IN ('Payed', 'Unpayed', 'Processing')),
+	tour_edition_id INT NOT NULL,
+	FOREIGN KEY (tour_edition_id) REFERENCES TourEdition(tour_edition_id),
+)
+GO
+
+CREATE TABLE Payment /* platnosc */
+(
+	payment_id INT IDENTITY(1,1) PRIMARY KEY, /* Generates primery key, from 1 with step 1 */
+	amount DECIMAL(10, 2) NOT NULL 
+		CHECK (amount > 0),
+	form_of_payment NVARCHAR(50) NOT NULL	
+		CHECK (form_of_payment IN ('Credit Card', 'Transfer', 'Cash')),
+	date_of_payment DATE NOT NULL,
+	reservation_id INT NOT NULL,
+	FOREIGN KEY (reservation_id) REFERENCES Reservation(reservation_id)
+)
+GO
+
+CREATE TABLE ReservationClient (
+    reservation_id INT NOT NULL,
+    client_pesel CHAR(11) NOT NULL,
+    PRIMARY KEY (reservation_id, client_pesel),
+    FOREIGN KEY (reservation_id) REFERENCES Reservation(reservation_id),
+    FOREIGN KEY (client_pesel) REFERENCES Client(client_pesel)
+);
+GO
+
+CREATE TABLE ReservationWorker (
+	reservation_id INT NOT NULL,
+	worker_pesel CHAR(11) NOT NULL,
+	PRIMARY KEY (reservation_id, worker_pesel),
+	FOREIGN KEY (reservation_id) REFERENCES Reservation(reservation_id),
+	FOREIGN KEY (worker_pesel) REFERENCES Worker(worker_pesel)
+);
+GO
