c0e2af997143867d491005f6d83fceec4718be67
Marta Kociszewska <martakociszewska04@gmail.com>
2025-10-29 21:00:06 +0100

    query to show change of workers and clients

---
+++ task2/sql_query/query4.sql
@@ -0,0 +1,17 @@
+USE go_explore_travel_agency;
+GO
+
+SELECT COUNT(*) AS liczba_klientow
+FROM Client
+
+SELECT COUNT(*) AS liczba_pracownikow
+FROM Worker
+
+USE go_explore_travel_agency_t2;
+GO
+
+SELECT COUNT(*) AS liczba_klientow
+FROM Client
+
+SELECT COUNT(*) AS liczba_pracownikow
+FROM Worker
