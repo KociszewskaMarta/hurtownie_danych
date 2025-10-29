4fce8b9e18d4ad50183efbbed4c6d6d3cea55ea8
Marta Kociszewska <martakociszewska04@gmail.com>
2025-10-29 20:32:27 +0100

    add header

--- task2/BULK_loading/load_database.sql
+++ task2/BULK_loading/load_database.sql
@@ -1,31 +1,30 @@
+USE go_explore_travel_agency;
+GO
+
 BULK INSERT dbo.Worker FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\workers.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-
 BULK INSERT dbo.Client FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\clients.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-
 BULK INSERT dbo.Tour FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\tours.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-
 BULK INSERT dbo.TourEdition FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\tour_editions.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-
 BULK INSERT dbo.Reservation FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\reservations.bulk'
 WITH (
     FIELDTERMINATOR = '|',
