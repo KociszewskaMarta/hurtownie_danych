2ec985619103f0577e9a86cb6b1d3df6eb352d46
Marta Kociszewska <martakociszewska04@gmail.com>
2025-10-29 18:03:31 +0100

    load all csv files

--- task2/BULK_loading/load_database.sql
+++ task2/BULK_loading/load_database.sql
@@ -11,3 +11,43 @@ WITH (
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
+
+BULK INSERT dbo.Tour FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\tours.bulk'
+WITH (
+    FIELDTERMINATOR = '|',
+    CODEPAGE = '65001',
+    DATAFILETYPE = 'char'
+);
+
+BULK INSERT dbo.TourEdition FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\tour_editions.bulk'
+WITH (
+    FIELDTERMINATOR = '|',
+    CODEPAGE = '65001',
+    DATAFILETYPE = 'char'
+);
+
+BULK INSERT dbo.Reservation FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\reservations.bulk'
+WITH (
+    FIELDTERMINATOR = '|',
+    CODEPAGE = '65001',
+    DATAFILETYPE = 'char'
+);
+
+BULK INSERT dbo.Payment FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\payments.bulk'
+WITH (
+    FIELDTERMINATOR = '|',
+    CODEPAGE = '65001',
+    DATAFILETYPE = 'char'
+);
+BULK INSERT dbo.ReservationClient FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\reservation_clients.bulk'
+WITH (
+    FIELDTERMINATOR = '|',
+    CODEPAGE = '65001',
+    DATAFILETYPE = 'char'
+);
+BULK INSERT dbo.ReservationWorker FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\reservation_workers.bulk'
+WITH (
+    FIELDTERMINATOR = '|',
+    CODEPAGE = '65001',
+    DATAFILETYPE = 'char'
+);
