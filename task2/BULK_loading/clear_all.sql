f51ed1e7c32d0272401894d8005bac29a8f021e0
Marta Kociszewska <martakociszewska04@gmail.com>
2025-10-29 16:12:16 +0100

    query to clera database

---
+++ task2/BULK_loading/clear_all.sql
@@ -0,0 +1,8 @@
+DELETE FROM dbo.Payment;
+DELETE FROM dbo.ReservationClient;
+DELETE FROM dbo.ReservationWorker;
+DELETE FROM dbo.Reservation;
+DELETE FROM dbo.TourEdition;
+DELETE FROM dbo.Tour;
+DELETE FROM dbo.Client;
+DELETE FROM dbo.Worker;
