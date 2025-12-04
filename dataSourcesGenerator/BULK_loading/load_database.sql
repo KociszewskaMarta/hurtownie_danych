b3c17450720e6c532d147a860bdfbd5df0c22e21
Marta Kociszewska <martakociszewska04@gmail.com>
2025-12-04 02:57:04 +0100

    Update BULK INSERT paths in load_database.sql and load_new_records.sql to reflect new directory structure and add CODEPAGE option

--- dataSourcesGenerator/BULK_loading/load_database.sql
+++ dataSourcesGenerator/BULK_loading/load_database.sql
@@ -1,50 +1,50 @@
 USE go_explore_travel_agency;
 GO
 
-BULK INSERT dbo.Worker FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\workers.bulk'
+BULK INSERT dbo.Worker FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\dataSourcesGenerator\python_scripts\generating_data\generating_bulk_files\data\workers.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-BULK INSERT dbo.Client FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\clients.bulk'
+BULK INSERT dbo.Client FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\dataSourcesGenerator\python_scripts\generating_data\generating_bulk_files\data\clients.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-BULK INSERT dbo.Tour FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\tours.bulk'
+BULK INSERT dbo.Tour FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\dataSourcesGenerator\python_scripts\generating_data\generating_bulk_files\data\tours.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-BULK INSERT dbo.TourEdition FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\tour_editions.bulk'
+BULK INSERT dbo.TourEdition FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\dataSourcesGenerator\python_scripts\generating_data\generating_bulk_files\data\tour_editions.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-BULK INSERT dbo.Reservation FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\reservations.bulk'
+BULK INSERT dbo.Reservation FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\dataSourcesGenerator\python_scripts\generating_data\generating_bulk_files\data\reservations.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
 
-BULK INSERT dbo.Payment FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\payments.bulk'
+BULK INSERT dbo.Payment FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\dataSourcesGenerator\python_scripts\generating_data\generating_bulk_files\data\payments.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-BULK INSERT dbo.ReservationClient FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\reservation_clients.bulk'
+BULK INSERT dbo.ReservationClient FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\dataSourcesGenerator\python_scripts\generating_data\generating_bulk_files\data\reservation_clients.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
     DATAFILETYPE = 'char'
 );
-BULK INSERT dbo.ReservationWorker FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\task2\python_scripts\generating_data\generating_bulk_files\data\reservation_workers.bulk'
+BULK INSERT dbo.ReservationWorker FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\dataSourcesGenerator\python_scripts\generating_data\generating_bulk_files\data\reservation_workers.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     CODEPAGE = '65001',
