4e86bdf22156a19182679dd131d0e95e8745d6b6
Marta Kociszewska <martakociszewska04@gmail.com>
2026-01-09 18:24:29 +0100

    add SQL script to delete records from sample travel agency database

---
+++ ETL/task/sql_queries/sample_sources/delete_records_sample_database.sql
@@ -0,0 +1,11 @@
+USE sample_travel_agency_database
+GO
+
+DELETE FROM dbo.Payment;
+DELETE FROM dbo.ReservationClient;
+DELETE FROM dbo.ReservationWorker;
+DELETE FROM dbo.Reservation;
+DELETE FROM dbo.TourEdition;
+DELETE FROM dbo.Tour;
+DELETE FROM dbo.Client;
+DELETE FROM dbo.Worker;
