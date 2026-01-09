9120fe17613ec8a1d0ff36df85bb9a136a1ed67a
Marta Kociszewska <martakociszewska04@gmail.com>
2026-01-09 18:25:18 +0100

    add temporary marketing table and bulk insert for marketing data

--- ETL/task/sql_queries/loading_tables/load_rezerwacja_fact.sql
+++ ETL/task/sql_queries/loading_tables/load_rezerwacja_fact.sql
@@ -1,6 +1,32 @@
 USE sample_warehouse
 GO
 
+IF OBJECT_ID('dbo.Marketing_Temp') IS NOT NULL DROP TABLE dbo.Marketing_Temp;
+GO
+CREATE TABLE Marketing_Temp (
+    [Date] NVARCHAR(50),
+    Campaing_Name NVARCHAR(255),
+    [Ad-group] NVARCHAR(255),
+    Trip_id NVARCHAR(50),
+    Keyword NVARCHAR(255),
+    Impressions NVARCHAR(50),
+    Clicks NVARCHAR(50),
+    CTR NVARCHAR(50),
+    Cost NVARCHAR(50),
+    [Conversion Rate] NVARCHAR(50)
+);
+GO
+BULK INSERT Marketing_Temp
+FROM 'C:\Users\kocis\Desktop\SEM_5\Hurtownie_danych\Labolatoria\repo\hurtownie_danych\ETL\task\sql_queries\sample_sources\sample_marketing_data_ready.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TABLOCK,
+    CODEPAGE = '65001'
+);
+GO
+
 INSERT INTO Rezerwacja_F (
     id_wycieczki,
     id_nazwy_kampanii,
@@ -54,4 +80,7 @@ WHERE wyc.id_wycieczki IS NOT NULL
     AND dat.id_daty IS NOT NULL 
     AND junk.id_junk IS NOT NULL
     AND kamp.id_nazwy_kampanii IS NOT NULL;
+GO
+
+DROP TABLE dbo.Marketing_Temp;
 GO
