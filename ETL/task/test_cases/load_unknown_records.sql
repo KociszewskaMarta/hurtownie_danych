fff99da123e46b899b4529166db8089b7869073c
Marta Kociszewska <martakociszewska04@gmail.com>
2026-01-10 11:15:06 +0100

    script to load unknow rows for test case 6

--- ETL/task/test_cases/load_unknown_records.sql
+++ ETL/task/test_cases/load_unknown_records.sql
@@ -1,20 +1,23 @@
 USE sample_travel_agency_database_2;
 GO
 
--- Add a new client if needed
-INSERT INTO Client (client_pesel, first_name, last_name, email, phone_number)
-VALUES ('UNKNOWN', 'Test', 'UnknownDate', 'test.unknown@email.com', '000000000');
-GO
-
 -- Add a new reservation with a date that does NOT exist in Data_D
 INSERT INTO Reservation (reservation_date, reservation_status, tour_edition_id)
-VALUES ('2099-01-01', 'Paid', 1); -- 2099-01-01 is outside your loaded date range
+VALUES ('2098-01-01', 'Unpaid', 1); -- 2098-01-01 is outside your loaded date range
 GO
 
--- Get the new reservation_id
-DECLARE @new_res_id INT = SCOPE_IDENTITY();
+-- Add a new reservation referencing a client that does NOT exist in Klient_D
+DECLARE @new_res_id INT;
 
--- Link the reservation to the client
+INSERT INTO Reservation (reservation_date, reservation_status, tour_edition_id)
+VALUES ('2024-01-01', 'Unpaid', 1);
+
+SET @new_res_id = SCOPE_IDENTITY();
+
+INSERT INTO Client (client_pesel, first_name, last_name, email, phone_number)
+VALUES ('00000000000', 'Unknown', 'Unknown', 'unknown@email.com', '000000000');
+
 INSERT INTO ReservationClient (reservation_id, client_pesel)
-VALUES (@new_res_id, '99999999999');
+VALUES (@new_res_id, '00000000000'); -- Pesel that does not exist in Klient_D
 GO
+
