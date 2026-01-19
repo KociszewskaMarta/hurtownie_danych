4528326bd4b0eff2a581a88dc76f8c0033e93cb1
Marta Kociszewska <martakociszewska04@gmail.com>
2026-01-19 02:00:31 +0100

    Add truncate_all.sql script to clear all relevant tables in database_travel_agency

---
+++ dataSourcesGenerator/BULK_loading/truncate_all.sql
@@ -0,0 +1,10 @@
+USE database_travel_agency
+
+TRUNCATE TABLE dbo.Payment;
+TRUNCATE TABLE dbo.ReservationClient;
+TRUNCATE TABLE dbo.ReservationWorker;
+TRUNCATE TABLE dbo.Reservation;
+TRUNCATE TABLE dbo.TourEdition;
+TRUNCATE TABLE dbo.Tour;
+TRUNCATE TABLE dbo.Client;
+TRUNCATE TABLE dbo.Worker;
